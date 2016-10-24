9271b64df76109b57af5be9133cba5191e6ca0b8
Etxahun <etxahun.sanchez@gmail.com>
2016-10-25 00:53:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./config')
+
+// ========================= CONEXIÓN A MONGODB =========================================
+mongoose.connect(config.db, (err, res) => {
+  if (err) {
+    return console.log(`Error al conectar con la base de datos: ${err}`)
+  }
+  console.log('Conexión a la base de datos establecida!')
+
+  app.listen(config.port, () => {
+    // ES6: Hemos utilizado la "arrow function" para sustituir "function()" por "()=>"
+    // ES6: Para poder utilizar ${port} tendremos que sustituir los delimitadores de strings '' por `` (acentos invertidos que están al lado de la letra P)
+    console.log(`API REST corriendo en un http://localhost:${config.port}`)
+  })
+})
